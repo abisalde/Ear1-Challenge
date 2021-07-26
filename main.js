5920abd1de5fc0381255e23a6e21614a8510b283
abisalde <abisalde>
2021-07-26 03:10:20 -0400

    redit

--- main.js
+++ main.js
@@ -2,50 +2,24 @@
  * ======= NAVBAR TOGGLE CODE
  */
 
-let mainNav = document.getElementById("nav-menu");
-let navBarToggle = document.getElementById("navbar-toggle-menu");
+let mainNav = document.getElementById('nav-menu');
+let navBarToggle = document.getElementById('navbar-toggle-menu');
 
-navBarToggle.addEventListener("click", function() {
-    mainNav.classList.toggle("active");
+navBarToggle.addEventListener('click', function () {
+    mainNav.classList.toggle('active');
 });
 
 /** ADD FLEX-DIRECTION TO NAV */
 
-let direction = document.querySelector('#navbar-toggle-menu').addEventListener('click', () => {
-    document.querySelector('#menu').classList.toggle('new-menu');
-});
+let direction = document
+    .querySelector('#navbar-toggle-menu')
+    .addEventListener('click', () => {
+        document.querySelector('#menu').classList.toggle('new-menu');
+    });
 
 /** LET REMOVE THE EVENT LISTENER ON THE NAV */
-let remove = document.querySelector('#navbar-toggle-menu').removeEventListener('click', () => {
-    document.querySelector('#menu').classList.toogle('new-menu');
-});
-
-
-/*===========
-*============ FAQS ACCORDION
-*/
-
-// const enableAccordion = (elem) => {
-//     const panelClick = (e) => {
-//         displayPanel(e.currenTarget);
-//     }
-
-//     const displayPanel = (p) => {
-//         var exPanel = elem.querySelector('.active')
-//     if (exPanel) {
-//     exPanel.classList.remove('active');
-//     }
-//     p.classList.add("active");
-//     }
-//     let allAccordion = elem.querySelectorAll('.accordion-main');
-//     for (let i = 0; i < allAccordion.length; i++ ) {
-//     allAccordion[i].addEventListener('click', panelClick);
-//     }
-//     displayPanel(allAccordion[0]);
-// }
-// enableAccordion(document.getElementById('faqs-accordion'));
-
-
-$("input").intlTelInput({
-    utilsScript: "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/8.4.6/js/utils.js"
-});
+let remove = document
+    .querySelector('#navbar-toggle-menu')
+    .removeEventListener('click', () => {
+        document.querySelector('#menu').classList.toogle('new-menu');
+    });
